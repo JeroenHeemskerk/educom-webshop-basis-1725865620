1309e70a9e07e8e6f3d670d9c6522bcdb1d1fa50
Lonne Olmeyer <lonneolmeyer@gmail.com>
2024-09-23 09:44:35 +0200

    Resource leaks opgelost #30

--- file_repository.php
+++ file_repository.php
@@ -3,25 +3,34 @@
 function getUser(string $email): ?array
 {
     $userData = fopen("users/users.txt", "r");
-    while (!feof($userData)){
-        $line = fgets($userData);
-        $parts = explode ('|', $line);
-        if($parts[0] == $email) {
-            $foundUser = array('email' => $parts[0], 'name' => $parts[1], 'password' => $parts[2]);
-            return $foundUser;
+    try {
+
+        while (!feof($userData)){
+            $line = fgets($userData);
+            $parts = explode ('|', $line);
+            if($parts[0] == $email) {
+                $foundUser = array('email' => $parts[0], 'name' => $parts[1], 'password' => $parts[2]);
+                return $foundUser;
+            }
         }
-        $foundUser = null;
-        return $foundUser;
+        return null;
         }
-        fclose($userData);
+        finally {
+            fclose($userData);
+        }
     }
 
 
-function saveUser(array $userArray): void{
+function saveUser(array $userArray): void
+{
     $newUser = transformArrayToRecord($userArray);
     $userData = fopen("users/users.txt", "a");
-    fwrite($userData, $newUser);
-    fclose($userData);
+    try {
+        fwrite($userData, $newUser);
+    }
+    finally {
+        fclose($userData);
+    }
 }
 
 function transformRecordToArray(string $userRecord): array
